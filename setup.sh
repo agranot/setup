513f1a134d4e7e564da3d2908cef25d6e5f059d2
Ubuntu <ubuntu@ip-172-31-41-236.us-west-2.compute.internal>
2013-07-15 19:06:48 +0000

    Added the installation of node.js. Hope this works next time.

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
+# Install nodejs
+sudo apt-get install -y nodejs
+
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
